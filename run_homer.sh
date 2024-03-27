025a9d22db45469d87ed71a0b2848795fa6f0058
jlchen5 <jl_chen1@outlook.com>
2024-03-27 13:18:22 +0100

    Update run_homer.sh

--- run_homer.sh
+++ run_homer.sh
@@ -2,6 +2,9 @@
 
 mkdir -p 
 ls *_[12].bed |sed 's/_[12].bed//' |sort -u |while read line ;do 
-	mkdir -p ../motif/$line/
-	findMotifsGenome.pl ${line}_1.bed hg38 ../motif/$line/ -size 200 -mask 
+	mkdir -p ../motif/${line}_1/
+	findMotifsGenome.pl ${line}_1.bed hg38 ../motif/${line}_1/ -size 200 -mask 
+	mkdir -p ../motif/${line}_2/
+ 	findMotifsGenome.pl ${line}_2.bed hg38 ../motif/${line}_2/ -size 200 -mask 
+  
 done 
